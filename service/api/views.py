9c74a70af0d9c701d8860a0f96879efe1ca2d5f1
Richard Meissner <msc.meissner@gmail.com>
2018-03-02 17:59:47 +0100

    Added logging

--- service/api/views.py
+++ service/api/views.py
@@ -81,8 +81,9 @@ def execute_tx(request):
 
     purchase = check_subscription_token(settings.ANDROID_PRODUCT_ID, token)
     current_time = int(round(time.time() * 1000))
-    purchase_time_str = purchase.get("expiryTimeMillis")
-    if not purchase_time_str or int(purchase_time_str) < current_time:
+    expiry_time_str = purchase.get("expiryTimeMillis")
+    print("Expiry time: " + expiry_time_str)
+    if not expiry_time_str or int(expiry_time_str) < current_time:
         return Response({"error": "no active subscription"}, 401)
 
     return Response({"hash": _send_transaction(target, data=data)})
